6ab03821e31cbcbd64a9146bf39d05030b10f24c
supernovahs <harshitsinghal252@gmail.com>
2023-03-29 16:31:39 +0530

    ⚡What is salt in hashing ?

---
+++ Salt/Salt.circom
@@ -0,0 +1,33 @@
+pragma circom 2.1.4;
+
+
+
+// In this exercise, we will learn an important concept related to hashing . There are 2 values a , and b  . You want to 
+// perform computation on these and verify it , but secretly without discovering the values. 
+// One way is to hash the 2 values and then store the hash as a reference. 
+// There is on problem in this concept , attacker can brute force the 2 variables by comparing the public hash with the resulting hash.
+// To overcome this , we use a secret value in the input privately. We hash it with a and b. 
+
+// This way brute force becomes illogical as the cost will increase multifolds for the attacker.
+
+
+// Input 2 values, a and b . 
+// Alos input a var 'salt' .
+// Hash all 3 using mimcsponge as a hashing mechanism . 
+// Output the res using 'out'.
+
+template Salt() {
+    
+// Your code here..
+
+}
+
+component main  = Salt();
+// By default all inputs are private in circom. We will not define any input as public 
+// because we want them to be a secret , at least in this case. 
+
+// There will be cases where some values will be declared explicitly public .
+
+
+
+
---
+++ test/Salt.js
@@ -0,0 +1,36 @@
+const chai = require('chai');
+const { wasm } = require('circom_tester');
+const path = require("path");
+const F1Field = require("ffjavascript").F1Field;
+const Scalar = require("ffjavascript").Scalar;
+exports.p = Scalar.fromString("21888242871839275222246405745257275088548364400416034343698204186575808495617");
+const Fr = new F1Field(exports.p);
+
+const wasm_tester = require("circom_tester").wasm;
+
+const assert = chai.assert;
+
+describe(" mimcSponge  ", function (){
+    this.timeout(100000);
+
+    it("Should Hash the input using mimcsponge with a secret salt", async()=>{
+        const circuit = await wasm_tester(path.join(__dirname,"../Salt","Salt.circom"));
+        await circuit.loadConstraints();
+        let witness ; 
+        // 
+        const expectedOutput = 9298463176047380197626371459244539548819181814394649924288766436865924479390n;
+        
+        witness = await circuit.calculateWitness({"a":"4","b":"4","salt":"2"},true);
+        console.log((witness[1]));
+        console.log(Fr.e(expectedOutput));
+        assert(Fr.eq(Fr.e(witness[0]), Fr.e(1)));
+        assert(Fr.eq(Fr.e(witness[1]), Fr.e(expectedOutput)));
+
+        witness = await circuit.calculateWitness({"a":"45","b":"1001","salt":"0099"},true);
+        
+        const expectedOutput2 = 17165551905088735098668729485755579194569447435279436338955576584338338632036n;
+        assert(Fr.eq(Fr.e(witness[0]), Fr.e(1)));
+        assert(Fr.eq(Fr.e(witness[1]), Fr.e(expectedOutput2)));
+    
+    })
+})
