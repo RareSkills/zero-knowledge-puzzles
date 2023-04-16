1d8d766f65a3a54443015956f01e0eadc01c2d34
AymericRT <aymerictaylor@gmail.com>
2023-04-16 14:44:39 +0700

    UI fixes
    simple UI fixes

--- Sujiko/Frontend/pages/_app.tsx
+++ Sujiko/Frontend/pages/_app.tsx
@@ -1,43 +1,48 @@
-import '@rainbow-me/rainbowkit/styles.css';
-import {
-  getDefaultWallets,
-  RainbowKitProvider,
-} from '@rainbow-me/rainbowkit';
-import { ChakraProvider } from '@chakra-ui/react'
-import { configureChains, createClient, goerli, WagmiConfig } from 'wagmi';
-import { mainnet, polygon, optimism, arbitrum } from 'wagmi/chains';
-import { alchemyProvider } from 'wagmi/providers/alchemy';
-import { publicProvider } from 'wagmi/providers/public';
-import '@/styles/globals.css';
-import type { AppProps } from 'next/app'
+import "@rainbow-me/rainbowkit/styles.css";
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import { configureChains, createClient, goerli, WagmiConfig } from "wagmi";
+import { mainnet, polygon, optimism, arbitrum } from "wagmi/chains";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const theme = extendTheme({
+    styles: {
+      global: () => ({
+        body: {
+          bg: "",
+        },
+      }),
+    },
+  });
   const { chains, provider } = configureChains(
     [goerli],
     [
-      alchemyProvider({ apiKey: process.env.ALCHEMY_ID as string}),
-      publicProvider()
+      alchemyProvider({ apiKey: process.env.ALCHEMY_ID as string }),
+      publicProvider(),
     ]
   );
-  
+
   const { connectors } = getDefaultWallets({
-    appName: 'My RainbowKit App',
-    chains
+    appName: "My RainbowKit App",
+    chains,
   });
-  
+
   const wagmiClient = createClient({
     autoConnect: true,
     connectors,
-    provider
-  })
+    provider,
+  });
   return (
-    <ChakraProvider>
-
-    <WagmiConfig client={wagmiClient}>
-      <RainbowKitProvider chains={chains}>
-  <Component {...pageProps} />
-      </RainbowKitProvider>
-</WagmiConfig>
-</ChakraProvider>
-  )
+    <ChakraProvider theme={theme}>
+      <WagmiConfig client={wagmiClient}>
+        <RainbowKitProvider chains={chains}>
+          <Component {...pageProps} />
+        </RainbowKitProvider>
+      </WagmiConfig>
+    </ChakraProvider>
+  );
 }
